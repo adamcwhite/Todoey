810aba0875caf28bbf3e51baa044b47b876bcfea
Adam White <adam.c.white@icloud.com>
2019-09-09 17:09:59 +1000

    Revert Todo Item functionality to mark as done instead of delete when a Todo Item is selected

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -64,7 +64,7 @@ class TodoListViewController: UITableViewController {
         if let item = todoItems?[indexPath.row] {
             do {
                 try realm.write {
-                    realm.delete(item)
+                    item.done = !item.done
                 }
             } catch {
                 print("Error saving done status, \(error)")
